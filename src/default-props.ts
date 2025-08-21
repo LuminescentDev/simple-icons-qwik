9a3bd09ca84c12e21cc670d01e2a54d7d63aba43
saboooor <saboor547@gmail.com>
2025-08-20 22:27:26 -0600

    lmao forgor to get rid of this

--- src/default-props.ts
+++ src/default-props.ts
@@ -2,8 +2,4 @@ import { IconProps } from './icon-props';
 
 export default {
   size: 24,
-  color: 'currentColor',
-  strokeWidth: 2,
-  strokeLinecap: 'round',
-  strokeLinejoin: 'round',
-} as Required<IconProps>;
+} as IconProps;
--- src/icon-props.ts
+++ src/icon-props.ts
@@ -2,10 +2,6 @@ import { QwikDOMAttributes } from '@builder.io/qwik';
 
 export interface IconProps extends QwikDOMAttributes {
   size?: number,
-  color?: string,
-  strokeWidth?: number,
-  strokeLinecap?: 'round' | 'butt' | 'square' | 'inherit' | undefined,
-  strokeLinejoin?: 'round' | 'inherit' | 'miter' | 'bevel' | undefined
 }
 
 export interface BaseIconProps extends IconProps {
